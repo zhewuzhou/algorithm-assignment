8ffb1115e58e8fa6180693e8df897ee2d500c9ed
zhewuzhou <zhewuzhou@gmail.com>
2019-10-09 11:26:01 +0800

    [Z.Z][Week4] Start the work for week 4

---
+++ src/main/java/zhewuzhou/me/week4/Solver.java
@@ -0,0 +1,29 @@
+package zhewuzhou.me.week4;
+
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return false;
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+        return 0;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+
+    }
+}
