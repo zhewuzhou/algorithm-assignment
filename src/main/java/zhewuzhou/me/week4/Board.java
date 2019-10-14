876fbb998e5adf4a38b54e246690a4a831fcde1c
zhewuzhou <zhewuzhou@gmail.com>
2019-10-14 10:43:53 +0800

    [Z.Z][Week] refactoring the code

--- src/main/java/zhewuzhou/me/week4/Board.java
+++ src/main/java/zhewuzhou/me/week4/Board.java
@@ -3,29 +3,29 @@ package zhewuzhou.me.week4;
 import edu.princeton.cs.algs4.Stack;
 
 public class Board {
-    private int[][] myBlocks;
-    private int n;
+    private int[][] tiles;
+    private int dimension;
 
     public Board(int[][] blocks) {
-        this.n = blocks.length;
-        this.myBlocks = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                myBlocks[i][j] = blocks[i][j];
+        this.dimension = blocks.length;
+        this.tiles = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                tiles[i][j] = blocks[i][j];
             }
         }
     } // construct a board from an n-by-n array of blocks
 
     // (where blocks[i][j] = block in row i, column j)
     public int dimension() {
-        return this.n;
+        return this.dimension;
     } // board dimension n
 
     public int hamming() {
         int hammingSum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((myBlocks[i][j] != (i * n + j + 1)) && (myBlocks[i][j] != 0)) {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if ((tiles[i][j] != (i * dimension + j + 1)) && (tiles[i][j] != 0)) {
                     hammingSum++;
                 }
             }
@@ -36,12 +36,12 @@ public class Board {
     public int manhattan() {
         int manhattanSum = 0;
         int temp;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((myBlocks[i][j] != (i * n + j + 1)) && (myBlocks[i][j] != 0)) {
-                    temp = myBlocks[i][j] % n - 1;
-                    if (temp < 0) temp += n;
-                    manhattanSum = manhattanSum + (Math.abs(i - ((myBlocks[i][j] - 1) / n)) + Math
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if ((tiles[i][j] != (i * dimension + j + 1)) && (tiles[i][j] != 0)) {
+                    temp = tiles[i][j] % dimension - 1;
+                    if (temp < 0) temp += dimension;
+                    manhattanSum = manhattanSum + (Math.abs(i - ((tiles[i][j] - 1) / dimension)) + Math
                         .abs(j - temp));
                 }
             }
@@ -54,10 +54,10 @@ public class Board {
     } // is this board the goal board?
 
     public Board twin() {
-        int[][] twinBoard = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                twinBoard[i][j] = myBlocks[i][j];
+        int[][] twinBoard = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                twinBoard[i][j] = tiles[i][j];
             }
         }
         if ((twinBoard[0][0] != 0) && (twinBoard[0][1] != 0)) {
@@ -73,10 +73,10 @@ public class Board {
         if (y == null) return false;
         if (this.getClass() != y.getClass()) return false;
         Board temp = (Board) y;
-        if (this.n != temp.n) return false;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (this.myBlocks[i][j] != temp.myBlocks[i][j]) return false;
+        if (this.dimension != temp.dimension) return false;
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (this.tiles[i][j] != temp.tiles[i][j]) return false;
             }
         }
         return true;
@@ -84,16 +84,16 @@ public class Board {
 
     public Iterable<Board> neighbors() {
         int i0 = 0, j0 = 0;
-        int[][] temp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[i][j] = myBlocks[i][j];
+        int[][] temp = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                temp[i][j] = tiles[i][j];
             }
         }
         Stack<Board> neighbor = new Stack<Board>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (this.myBlocks[i][j] == 0) {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if (this.tiles[i][j] == 0) {
                     i0 = i;
                     j0 = j;
                 }
@@ -104,7 +104,7 @@ public class Board {
             neighbor.push(new Board(temp));
             exch(temp, i0, j0, i0 - 1, j0);
         }
-        if (i0 != (n - 1)) {
+        if (i0 != (dimension - 1)) {
             exch(temp, i0, j0, i0 + 1, j0);
             neighbor.push(new Board(temp));
             exch(temp, i0, j0, i0 + 1, j0);
@@ -114,7 +114,7 @@ public class Board {
             neighbor.push(new Board(temp));
             exch(temp, i0, j0, i0, j0 - 1);
         }
-        if (j0 != (n - 1)) {
+        if (j0 != (dimension - 1)) {
             exch(temp, i0, j0, i0, j0 + 1);
             neighbor.push(new Board(temp));
             exch(temp, i0, j0, i0, j0 + 1);
@@ -124,10 +124,10 @@ public class Board {
 
     public String toString() {
         StringBuilder s = new StringBuilder();
-        s.append(n + "\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                s.append(String.format("%2d ", myBlocks[i][j]));
+        s.append(dimension + "\n");
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
             }
             s.append("\n");
         }
