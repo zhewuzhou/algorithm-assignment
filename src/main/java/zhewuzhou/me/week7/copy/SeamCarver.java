2a73f3d8f4a92541df990aa25fed56a3d466eaec
zhewuzhou <zhewuzhou@gmail.com>
2019-10-28 23:38:37 +0800

    [Z.Z][week7] nicer solution

--- src/main/java/zhewuzhou/me/week7/copy/SeamCarver.java
+++ src/main/java/zhewuzhou/me/week7/copy/SeamCarver.java
@@ -5,116 +5,117 @@ import edu.princeton.cs.algs4.Picture;
 import java.awt.Color;
 
 public class SeamCarver {
-    private static final int R = 16;
-    private static final int G = 8;
-    private static final int B = 0;
+    private static final int RED = 16;
+    private static final int GREEN = 8;
+    private static final int BLUE = 0;
+    private static final int EIGHT_ONE = 0xff;
     private int[] vSeam, hSeam;
-    private int[][] c;
-    private double[][] energy;
-    private int W, H;
+    private int[][] colors;
+    private double[][] energies;
+    private int width, height;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         if (picture == null)
             throw new IllegalArgumentException();
-        W = picture.width();
-        H = picture.height();
-        energy = new double[H][W];
-        c = new int[H][W];
-        for (int j = 0; j < H; j++)
-            for (int i = 0; i < W; i++)
-                c[j][i] = picture.get(i, j).getRGB();
-        for (int j = 0; j < H; j++)
-            for (int i = 0; i < W; i++)
-                if (i == 0 || i == W - 1 || j == 0 || j == H - 1)
-                    energy[j][i] = 1000;
+        width = picture.width();
+        height = picture.height();
+        energies = new double[height][width];
+        colors = new int[height][width];
+        for (int j = 0; j < height; j++)
+            for (int i = 0; i < width; i++)
+                colors[j][i] = picture.get(i, j).getRGB();
+        for (int j = 0; j < height; j++)
+            for (int i = 0; i < width; i++)
+                if (i == 0 || i == width - 1 || j == 0 || j == height - 1)
+                    energies[j][i] = 1000;
                 else
                     calcEnergy(j, i);
 
     }
 
     private void calcEnergy(int row, int col) {
-        if (row <= 0 || row >= H - 1 || col <= 0 || col >= W - 1)
+        if (row <= 0 || row >= height - 1 || col <= 0 || col >= width - 1)
             return;
         int rx, gx, bx, ry, gy, by;
-        rx = getColor(row, col + 1, R) - getColor(row, col - 1, R);
-        gx = getColor(row, col + 1, G) - getColor(row, col - 1, G);
-        bx = getColor(row, col + 1, B) - getColor(row, col - 1, B);
-        ry = getColor((row + 1), col, R) - getColor((row - 1), col, R);
-        gy = getColor((row + 1), col, G) - getColor((row - 1), col, G);
-        by = getColor((row + 1), col, B) - getColor((row - 1), col, B);
-        energy[row][col] = Math.sqrt(rx * rx + gx * gx + bx * bx
+        rx = getColor(row, col + 1, RED) - getColor(row, col - 1, RED);
+        gx = getColor(row, col + 1, GREEN) - getColor(row, col - 1, GREEN);
+        bx = getColor(row, col + 1, BLUE) - getColor(row, col - 1, BLUE);
+        ry = getColor((row + 1), col, RED) - getColor((row - 1), col, RED);
+        gy = getColor((row + 1), col, GREEN) - getColor((row - 1), col, GREEN);
+        by = getColor((row + 1), col, BLUE) - getColor((row - 1), col, BLUE);
+        energies[row][col] = Math.sqrt(rx * rx + gx * gx + bx * bx
             + ry * ry + gy * gy + by * by);
     }
 
     private int getColor(int row, int col, int channel) {
-        return c[row][col] >> channel & 255;
+        return colors[row][col] >> channel & EIGHT_ONE;
     }
 
     // current picture
     public Picture picture() {
-        Picture pic = new Picture(W, H);
-        for (int j = 0; j < H; j++)
-            for (int i = 0; i < W; i++)
-                pic.set(i, j, new Color(c[j][i], true));
+        Picture pic = new Picture(width, height);
+        for (int j = 0; j < height; j++)
+            for (int i = 0; i < width; i++)
+                pic.set(i, j, new Color(colors[j][i], true));
         return pic;
     }
 
     // current picture
     public int width() {
-        return W;
+        return width;
     }
 
     // height of current picture
     public int height() {
-        return H;
+        return height;
     }
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x < 0 || x >= W || y < 0 || y >= H)
+        if (x < 0 || x >= width || y < 0 || y >= height)
             throw new IllegalArgumentException();
-        return energy[y][x];
+        return energies[y][x];
     }
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
         if (hSeam != null) return hSeam.clone();
-        int[] seam = new int[W];
-        if (W < 3) {
-            for (int i = 1; i < W; i++)
+        int[] seam = new int[width];
+        if (width < 3) {
+            for (int i = 1; i < width; i++)
                 seam[i] = 0;
             hSeam = seam;
             return seam.clone();
         }
 
-        double[] distTo = new double[H * W];
-        int[] edgeTo = new int[H * W];
-        for (int i = 0; i < H; i++)
-            for (int j = 1; j < W; j++)
-                distTo[i * W + j] = Double.POSITIVE_INFINITY;
-        for (int i = 1; i < W; i++) { // Main Loop
-            for (int j = 0; j < H; j++) {
-                if (j > 0 && distTo[(j - 1) * W + i] > energy[j - 1][i] + distTo[j * W + i - 1]) { // Up
-                    distTo[(j - 1) * W + i] = energy[j - 1][i] + distTo[j * W + i - 1];
-                    edgeTo[(j - 1) * W + i] = j;
+        double[] energyTo = new double[height * width];
+        int[] pathTo = new int[height * width];
+        for (int i = 0; i < height; i++)
+            for (int j = 1; j < width; j++)
+                energyTo[i * width + j] = Double.POSITIVE_INFINITY;
+        for (int i = 1; i < width; i++) { // Main Loop
+            for (int j = 0; j < height; j++) {
+                if (j > 0 && energyTo[(j - 1) * width + i] > energies[j - 1][i] + energyTo[j * width + i - 1]) { // Up
+                    energyTo[(j - 1) * width + i] = energies[j - 1][i] + energyTo[j * width + i - 1];
+                    pathTo[(j - 1) * width + i] = j;
                 }
-                if (distTo[j * W + i] > energy[j][i] + distTo[j * W + i - 1]) { // Middle
-                    distTo[j * W + i] = energy[j][i] + distTo[j * W + i - 1];
-                    edgeTo[j * W + i] = j;
+                if (energyTo[j * width + i] > energies[j][i] + energyTo[j * width + i - 1]) { // Middle
+                    energyTo[j * width + i] = energies[j][i] + energyTo[j * width + i - 1];
+                    pathTo[j * width + i] = j;
                 }
-                if (j < H - 1 && distTo[(j + 1) * W + i] > energy[j + 1][i] + distTo[j * W + i - 1]) { // Down
-                    distTo[(j + 1) * W + i] = energy[j + 1][i] + distTo[j * W + i - 1];
-                    edgeTo[(j + 1) * W + i] = j;
+                if (j < height - 1 && energyTo[(j + 1) * width + i] > energies[j + 1][i] + energyTo[j * width + i - 1]) { // Down
+                    energyTo[(j + 1) * width + i] = energies[j + 1][i] + energyTo[j * width + i - 1];
+                    pathTo[(j + 1) * width + i] = j;
                 }
             }
         }
-        for (int i = 1; i < H - 1; i++)
-            if (distTo[seam[W - 2] * W + W - 2] > distTo[i * W + W - 2])
-                seam[W - 2] = i;
-        seam[W - 1] = seam[W - 2];
-        for (int i = W - 3; i >= 0; i--)
-            seam[i] = edgeTo[seam[i + 1] * W + i + 1];
+        for (int i = 1; i < height - 1; i++)
+            if (energyTo[seam[width - 2] * width + width - 2] > energyTo[i * width + width - 2])
+                seam[width - 2] = i;
+        seam[width - 1] = seam[width - 2];
+        for (int i = width - 3; i >= 0; i--)
+            seam[i] = pathTo[seam[i + 1] * width + i + 1];
 
         hSeam = seam;
         return seam.clone();
@@ -123,41 +124,41 @@ public class SeamCarver {
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
         if (vSeam != null) return vSeam.clone();
-        int[] seam = new int[H];
-        if (H < 3) {
-            for (int i = 1; i < H; i++)
+        int[] seam = new int[height];
+        if (height < 3) {
+            for (int i = 1; i < height; i++)
                 seam[i] = 0;
             vSeam = seam;
             return seam.clone();
         }
 
-        double[] distTo = new double[H * W];
-        int[] edgeTo = new int[H * W];
-        for (int j = 1; j < H; j++)
-            for (int i = 0; i < W; i++)
-                distTo[j * W + i] = Double.POSITIVE_INFINITY;
-        for (int j = 1; j < H; j++) { // Main Loop
-            for (int i = 0; i < W; i++) {
-                if (i > 0 && distTo[j * W + i - 1] > energy[j][i - 1] + distTo[(j - 1) * W + i]) { // Left
-                    distTo[j * W + i - 1] = energy[j][i - 1] + distTo[(j - 1) * W + i];
-                    edgeTo[j * W + i - 1] = i;
+        double[] energyTo = new double[height * width];
+        int[] pathTo = new int[height * width];
+        for (int j = 1; j < height; j++)
+            for (int i = 0; i < width; i++)
+                energyTo[j * width + i] = Double.POSITIVE_INFINITY;
+        for (int j = 1; j < height; j++) { // Main Loop
+            for (int i = 0; i < width; i++) {
+                if (i > 0 && energyTo[j * width + i - 1] > energies[j][i - 1] + energyTo[(j - 1) * width + i]) { // Left
+                    energyTo[j * width + i - 1] = energies[j][i - 1] + energyTo[(j - 1) * width + i];
+                    pathTo[j * width + i - 1] = i;
                 }
-                if (distTo[j * W + i] > energy[j][i] + distTo[(j - 1) * W + i]) { // Middle
-                    distTo[j * W + i] = energy[j][i] + distTo[(j - 1) * W + i];
-                    edgeTo[j * W + i] = i;
+                if (energyTo[j * width + i] > energies[j][i] + energyTo[(j - 1) * width + i]) { // Middle
+                    energyTo[j * width + i] = energies[j][i] + energyTo[(j - 1) * width + i];
+                    pathTo[j * width + i] = i;
                 }
-                if (i < W - 1 && distTo[j * W + i + 1] > energy[j][i + 1] + distTo[(j - 1) * W + i]) { // Right
-                    distTo[j * W + i + 1] = energy[j][i + 1] + distTo[(j - 1) * W + i];
-                    edgeTo[j * W + i + 1] = i;
+                if (i < width - 1 && energyTo[j * width + i + 1] > energies[j][i + 1] + energyTo[(j - 1) * width + i]) { // Right
+                    energyTo[j * width + i + 1] = energies[j][i + 1] + energyTo[(j - 1) * width + i];
+                    pathTo[j * width + i + 1] = i;
                 }
             }
         }
-        for (int i = 1; i < W - 1; i++)
-            if (distTo[(H - 2) * W + seam[H - 2]] > distTo[(H - 2) * W + i])
-                seam[H - 2] = i;
-        seam[H - 1] = seam[H - 2];
-        for (int i = H - 3; i >= 0; i--)
-            seam[i] = edgeTo[(i + 1) * W + seam[i + 1]];
+        for (int i = 1; i < width - 1; i++)
+            if (energyTo[(height - 2) * width + seam[height - 2]] > energyTo[(height - 2) * width + i])
+                seam[height - 2] = i;
+        seam[height - 1] = seam[height - 2];
+        for (int i = height - 3; i >= 0; i--)
+            seam[i] = pathTo[(i + 1) * width + seam[i + 1]];
 
         vSeam = seam;
         return seam.clone();
@@ -165,73 +166,73 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
-        if (H <= 1)
+        if (height <= 1)
             throw new IllegalArgumentException();
         if (seam == null)
             throw new IllegalArgumentException();
-        if (seam.length != W)
+        if (seam.length != width)
             throw new IllegalArgumentException();
-        for (int i = 0; i < W; i++) {
+        for (int i = 0; i < width; i++) {
             if (i > 0 && Math.abs(seam[i] - seam[i - 1]) > 1)
                 throw new IllegalArgumentException();
-            if (seam[i] < 0 || seam[i] >= H)
+            if (seam[i] < 0 || seam[i] >= height)
                 throw new IllegalArgumentException();
         }
 
-        for (int i = 0; i < W; i++) {
-            if (seam[i] == H - 1) {
-                energy[H - 2][i] = 1000;
+        for (int i = 0; i < width; i++) {
+            if (seam[i] == height - 1) {
+                energies[height - 2][i] = 1000;
                 continue;
             }
-            for (int j = seam[i]; j < H - 1; j++) {
-                if (j == 0) energy[j][i] = 1000;
-                else energy[j][i] = energy[j + 1][i];
-                c[j][i] = c[j + 1][i];
+            for (int j = seam[i]; j < height - 1; j++) {
+                if (j == 0) energies[j][i] = 1000;
+                else energies[j][i] = energies[j + 1][i];
+                colors[j][i] = colors[j + 1][i];
             }
         }
-        H--;
+        height--;
         vSeam = null;
         hSeam = null;
-        for (int i = 1; i < W - 1; i++) {
+        for (int i = 1; i < width - 1; i++) {
             if (seam[i] > 1) calcEnergy(seam[i] - 1, i); // Up
-            if (seam[i] < H - 1) calcEnergy(seam[i], i); // Down
+            if (seam[i] < height - 1) calcEnergy(seam[i], i); // Down
             if (i > 1) calcEnergy(seam[i], i - 1); // Left
-            if (i < W - 2) calcEnergy(seam[i], i + 1); // Right
+            if (i < width - 2) calcEnergy(seam[i], i + 1); // Right
         }
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-        if (W <= 1)
+        if (width <= 1)
             throw new IllegalArgumentException();
         if (seam == null)
             throw new IllegalArgumentException();
-        if (seam.length != H)
+        if (seam.length != height)
             throw new IllegalArgumentException();
-        for (int i = 0; i < H; i++) {
+        for (int i = 0; i < height; i++) {
             if (i > 0 && Math.abs(seam[i] - seam[i - 1]) > 1)
                 throw new IllegalArgumentException();
-            if (seam[i] < 0 || seam[i] >= W)
+            if (seam[i] < 0 || seam[i] >= width)
                 throw new IllegalArgumentException();
         }
 
-        for (int i = 0; i < H; i++) {
-            if (seam[i] == W - 1) {
-                energy[i][W - 2] = 1000;
+        for (int i = 0; i < height; i++) {
+            if (seam[i] == width - 1) {
+                energies[i][width - 2] = 1000;
                 continue;
             }
-            System.arraycopy(energy[i], seam[i] + 1, energy[i], seam[i], W - seam[i] - 1);
-            System.arraycopy(c[i], seam[i] + 1, c[i], seam[i], W - seam[i] - 1);
-            if (seam[i] == 0) energy[i][0] = 1000;
+            System.arraycopy(energies[i], seam[i] + 1, energies[i], seam[i], width - seam[i] - 1);
+            System.arraycopy(colors[i], seam[i] + 1, colors[i], seam[i], width - seam[i] - 1);
+            if (seam[i] == 0) energies[i][0] = 1000;
         }
-        W--;
+        width--;
         vSeam = null;
         hSeam = null;
-        for (int i = 1; i < H - 1; i++) {
+        for (int i = 1; i < height - 1; i++) {
             if (seam[i] > 1) calcEnergy(i, seam[i] - 1); // Left
-            if (seam[i] < W - 1) calcEnergy(i, seam[i]); // Right
+            if (seam[i] < width - 1) calcEnergy(i, seam[i]); // Right
             if (i > 1) calcEnergy(i - 1, seam[i]); // Up
-            if (i < H - 2) calcEnergy(i + 1, seam[i]); // Down
+            if (i < height - 2) calcEnergy(i + 1, seam[i]); // Down
         }
     }
 }
