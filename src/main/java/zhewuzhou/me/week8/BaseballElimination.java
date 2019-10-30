e3a501d5fdbfa09d22d5dc77abafb5424eb536c9
zhewuzhou <zhewuzhou@gmail.com>
2019-10-30 23:16:41 +0800

    [Z.Z][Week8] Add new files

---
+++ src/main/java/zhewuzhou/me/week8/BaseballElimination.java
@@ -0,0 +1,46 @@
+package zhewuzhou.me.week8;
+
+public class BaseballElimination {
+    BaseballElimination(String filename) {
+    }
+
+    // number of teams
+    public int numberOfTeams() {
+        return 0;
+    }
+
+    // all teams
+    public Iterable<String> teams() {
+        return null;
+    }
+
+    // number of wins for given team
+    public int wins(String team) {
+        return 0;
+    }
+
+    // number of losses for given team
+    public int losses(String team) {
+        return 0;
+    }
+
+    // number of remaining games for given team
+    public int remaining(String team) {
+        return 0;
+    }
+
+    // number of remaining games between team1 and team2
+    public int against(String team1, String team2) {
+        return 0;
+    }
+
+    // is given team eliminated?
+    public boolean isEliminated(String team) {
+        return false;
+    }
+
+    // subset R of teams that eliminates given team; null if not eliminated
+    public Iterable<String> certificateOfElimination(String team) {
+        return null;
+    }
+}
