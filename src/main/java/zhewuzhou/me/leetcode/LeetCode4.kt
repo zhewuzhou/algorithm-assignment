379da55634bbbaf85d487b0c6513b8bad11e9bfc
Z.Z <zhewuzhou@gmail.com>
2020-02-04 21:50:15 +0800

    finish leetcode 4 algorithm is really hard to remember

--- src/main/java/zhewuzhou/me/leetcode/LeetCode4.kt
+++ src/main/java/zhewuzhou/me/leetcode/LeetCode4.kt
@@ -1,5 +1,46 @@
 package zhewuzhou.me.leetcode
 
+import java.lang.Integer.min
+
+/*
+A[a, b, c .. x]: median is the middle elem m
+B[a1, b1, c1, ..x1] median is the middle elem m1
+find mth element in 2 list:
+for x - 1 = i + j
+All B[0..j] < A[i] && A[0
+ */
+
 fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-    return 0.0
+    val totalSize = nums1.size + nums2.size
+    return if (totalSize % 2 == 0)
+        (findKthElement(nums1.toList(), nums2.toList(), totalSize / 2)
+                + findKthElement(nums1.toList(), nums2.toList(), totalSize / 2 + 1)) / 2.0
+    else findKthElement(nums1.toList(), nums2.toList(), totalSize / 2 + 1).toDouble()
 }
+
+fun findKthElement(l1: List<Int>, l2: List<Int>, k: Int): Int {
+    check(k <= l1.size + l2.size)
+    if (l2.first() > l1.last()) {
+        return (l1 + l2)[k - 1]
+    }
+    if (l1.first() > l2.last()) {
+        return (l2 + l1)[k - 1]
+    }
+    var i = 0;
+    var j = 0;
+    var iMin = 0;
+    var iMax = min(l1.size, k - 1)
+    while (iMin <= iMax) {
+        i = (iMin + iMax) / 2
+        j = k - 1 - i
+        if (j > 0 && l2[j - 1] > l1[i]) {
+            iMin = i + 1
+        } else if (i > 0 && l1[i - 1] > l2[j]) {
+            iMax = i - 1
+        } else {
+            return min(l1[i], l2[j])
+        }
+    }
+    return -1
+}
+
--- src/test/java/zhewuzhou/me/leetcode/LeetCode4KtTest.kt
+++ src/test/java/zhewuzhou/me/leetcode/LeetCode4KtTest.kt
@@ -9,7 +9,16 @@ class LeetCode4KtTest {
 
     @Test
     fun `should calculate medium for 2 lists`() {
-        val result = findMedianSortedArrays(listOf(1, 2).toIntArray(), listOf(3, 4).toIntArray())
-        assertThat(result, `is`(2.5))
+        //val result = findMedianSortedArrays(listOf(1, 2).toIntArray(), listOf(3, 4).toIntArray())
+        assertThat(findKthElement(listOf(1,2), listOf(3, 4), 3), `is`(3))
+        //assertThat(result, `is`(2.5))
+    }
+
+    @Test
+    fun `should fine the kth element from 2 list`(){
+        val the5Th = findKthElement(listOf(1, 2, 10, 12), listOf(3, 4, 8, 9), 5)
+        val the6Th = findKthElement(listOf(1, 2, 10, 12), listOf(3, 4, 8, 9), 6)
+        assertThat(the5Th, `is`(8))
+        assertThat(the6Th, `is`(9))
     }
 }
