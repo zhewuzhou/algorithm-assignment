16e4b0357fa3c7490c118682435719c9d93cf400
Z.Z <zhewuzhou@gmail.com>
2020-02-05 16:11:37 +0800

    n*n*n complixcity for the Palindrome substring problems

--- src/main/java/zhewuzhou/me/leetcode/LeetCode5.kt
+++ src/main/java/zhewuzhou/me/leetcode/LeetCode5.kt
@@ -1,8 +1,12 @@
 package zhewuzhou.me.leetcode
 
-fun longestPalindrome(s: String): String {
-    return ""
-}
+fun longestPalindrome(s: String): String =
+        (s.indices).flatMap { s.allSubStrings(it) }.filter { it.isPalindrome() }.maxBy { it.length } ?: ""
+
+
+fun String.allSubStrings(index: Int): List<String> =
+        (index + 1 until length).map { this.substring(index, it + 1) }
+
 
 fun String.isPalindrome(): Boolean =
         if (length < 2) false
--- src/test/java/zhewuzhou/me/leetcode/LeetCode5KtTest.kt
+++ src/test/java/zhewuzhou/me/leetcode/LeetCode5KtTest.kt
@@ -30,4 +30,19 @@ class LeetCode5KtTest {
     fun `single char should not be palindromic`() {
         assertThat("a".isPalindrome(), `is`(false))
     }
+
+    @Test
+    fun `should calculate all substrings for banana 0`() {
+        assertThat("banana".allSubStrings(0), `is`(listOf("ba", "ban", "bana", "banan", "banana")))
+    }
+
+    @Test
+    fun `should calculate all substrings for empty 0`() {
+        assertThat("".allSubStrings(0), `is`(listOf()))
+    }
+
+    @Test
+    fun `should calculate all substrings for single char 0`() {
+        assertThat("a".allSubStrings(0), `is`(listOf()))
+    }
 }
