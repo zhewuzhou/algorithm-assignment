085444307c4f89a77bfc912d97fc80102b6800b5
zhewuzhou <zhewuzhou@gmail.com>
2020-02-17 15:04:37 +0800

    [Z.Z][LeetCode4] recursive solution is very good for complex cases

--- src/main/java/zhewuzhou/me/leetcode/LeetCode4.kt
+++ src/main/java/zhewuzhou/me/leetcode/LeetCode4.kt
@@ -1,8 +1,5 @@
 package zhewuzhou.me.leetcode
 
-import java.lang.Integer.max
-import java.lang.Integer.min
-
 /*
 A[a, b, c .. x]: median is the middle elem m
 B[a1, b1, c1, ..x1] median is the middle elem m1
@@ -11,64 +8,22 @@ for x - 1 = i + j
 All B[0..j] < A[i] && A[0
  */
 
-fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-    val totalSize = nums1.size + nums2.size
-    return if (totalSize % 2 == 0)
-        (findKthElement(nums1.toList(), nums2.toList(), totalSize / 2)
-            + findKthElement(nums1.toList(), nums2.toList(), totalSize / 2 + 1)) / 2.0
-    else findKthElement(nums1.toList(), nums2.toList(), totalSize / 2 + 1).toDouble()
+fun findMedianSortedArrays(lhs: IntArray, rhs: IntArray): Double {
+    val total = lhs.size + rhs.size
+    return when (total % 2 == 0) {
+        (true) -> (kth(lhs, 0, rhs, 0, total / 2) + kth(lhs, 0, rhs, 0, (total / 2 + 1))) / 2.0
+        (false) -> kth(lhs, 0, rhs, 0, (total + 1) / 2).toDouble()
+    }
 }
 
-fun findKthElement(lhs: List<Int>, rhs: List<Int>, k: Int): Int {
-    if (k == lhs.size + rhs.size) {
-        return max(lhs.last(), rhs.last())
-    }
-    var start = 0
-    var end = min(lhs.size - 1, k - 1)
-    while (start <= end) {
-        val lPartition = (end + start) / 2
-        val rPartition = k - lPartition - 1
-        if (rPartition <= rhs.size - 1) {
-            val lhsValidSplit = when (rPartition > 0) {
-                (true) -> lPartition == lhs.size - 1 || lhs[lPartition] >= rhs[rPartition - 1]
-                (false) -> true
-            }
-            val rhsValidSplit = when (lPartition > 0) {
-                (true) -> lhs[lPartition - 1] <= rhs[rPartition]
-                (false) -> true
-            }
-            if (lhsValidSplit && rhsValidSplit) {
-                return kthByPartition(lhs, lPartition, rhs, rPartition)
-            }
-            if (lPartition > 0 && lhs[lPartition - 1] > rhs[rPartition]) {
-                end = lPartition - 1
-            } else {
-                start = lPartition + 1
-            }
-        } else {
-            start = lPartition + 1
-        }
-    }
-    return 0
-}
-
-private fun kthByPartition(lhs: List<Int>, lPartition: Int, rhs: List<Int>, rPartition: Int): Int {
-    if (lPartition == lhs.size - 1 && rPartition >= 1) {
-        if (lhs[lPartition] <= rhs[rPartition - 1]) {
-            return rhs[rPartition - 1]
-        }
-    }
-    if (rPartition == rhs.size - 1 && lPartition >= 1) {
-        if (rhs[rPartition] <= lhs[lPartition - 1]) {
-            return lhs[lPartition - 1]
-        }
-    }
-    return min(lhs[lPartition], rhs[rPartition])
-}
-
-private fun isOrdered(l2: List<Int>, l1: List<Int>): Boolean {
-    if (l2.first() >= l1.last()) {
-        return true
-    }
-    return false
+fun kth(lhs: IntArray, ls: Int, rhs: IntArray, rs: Int, k: Int): Int {
+    if (ls > lhs.size - 1) return rhs[rs + k - 1]
+    if (rs > rhs.size - 1) return lhs[ls + k - 1]
+    if (k == 1) return Math.min(lhs[ls], rhs[rs])
+    var aMid = Int.MAX_VALUE
+    var bMid = Int.MAX_VALUE
+    if (ls + k / 2 - 1 < lhs.size) aMid = lhs[ls + k / 2 - 1]
+    if (rs + k / 2 - 1 < rhs.size) bMid = rhs[rs + k / 2 - 1]
+    return if (aMid < bMid) kth(lhs, ls + k / 2, rhs, rs, k - k / 2) // Check: aRight + bLeft
+    else kth(lhs, ls, rhs, rs + k / 2, k - k / 2) // Check: bRight + aLeft
 }
