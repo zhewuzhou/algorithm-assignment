07168cdb873db3246c7a9938e342890ae5650de5
zhewuzhou <zhewuzhou@gmail.com>
2020-02-18 12:14:09 +0800

    [Z.Z][LeetCode10] Before cache the result

--- src/main/java/zhewuzhou/me/leetcode/LeetCode10.kt
+++ src/main/java/zhewuzhou/me/leetcode/LeetCode10.kt
@@ -1,27 +1,30 @@
 package zhewuzhou.me.leetcode
 
-fun isMatch(s: String, p: String): Boolean =
-    matchPosition(s, 0, p, 0)
-
-private fun matchPosition(src: String, sp: Int, pattern: String, pp: Int): Boolean {
-    if (sp >= src.length - 1) {        //src run out
-        return pp >= pattern.length - 1
-    }
-    if (pp >= pattern.length - 1) { //pattern run out
-        return false
-    }
-    return if (pattern[pp + 1] == '*') {
-        when (pattern[pp] == '.' || src[sp] == pattern[pp]) {
-            (true) -> (matchPosition(src, sp, pattern, pp + 2)
-                || matchPosition(src, sp + 1, pattern, pp)
-                || matchPosition(src, sp + 1, pattern, pp + 2))
-            (false) -> matchPosition(src, sp, pattern, pp + 2)
+fun isMatch(s: String, p: String): Boolean {
+    val cache = mutableMapOf<Pair<Int, Int>, Boolean>()
+    fun matchPosition(src: String, sp: Int, pattern: String, pp: Int): Boolean {
+        if (sp >= src.length - 1) {        //src run out
+            return pp >= pattern.length - 1
         }
+        if (pp >= pattern.length - 1) { //pattern run out
+            return false
+        }
+        return if (pattern[pp + 1] == '*') {
+            when (pattern[pp] == '.' || src[sp] == pattern[pp]) {
+                (true) -> (matchPosition(src, sp, pattern, pp + 2)
+                    || matchPosition(src, sp + 1, pattern, pp)
+                    || matchPosition(src, sp + 1, pattern, pp + 2))
+                (false) -> matchPosition(src, sp, pattern, pp + 2)
+            }
 
-    } else {
-        when (pattern[pp] == '.' || pattern[pp] == src[sp]) {
-            (true) -> matchPosition(src, sp + 1, pattern, pp + 1)
-            (false) -> false
+        } else {
+            when (pattern[pp] == '.' || pattern[pp] == src[sp]) {
+                (true) -> matchPosition(src, sp + 1, pattern, pp + 1)
+                (false) -> false
+            }
         }
     }
+    return matchPosition(s, 0, p, 0)
 }
+
+
