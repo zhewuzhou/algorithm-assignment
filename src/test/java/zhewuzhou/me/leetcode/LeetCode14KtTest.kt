73a579bbae547907f1df18ef1ce1dd6300c2d23e
zhewuzhou <zhewuzhou@gmail.com>
2020-02-21 13:16:27 +0800

    [Z.Z][LeetCode14] finish 14 with quick and dirty solution

---
+++ src/test/java/zhewuzhou/me/leetcode/LeetCode14KtTest.kt
@@ -0,0 +1,20 @@
+package zhewuzhou.me.leetcode
+
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class LeetCode14KtTest {
+
+    @Test
+    fun `Should calculate the possible prefix for all simple strings`() {
+        val stringArrays = listOf("flower", "flow", "flight").toTypedArray()
+        assertThat(longestCommonPrefix(stringArrays), `is`("fl"))
+    }
+
+    @Test
+    fun `Should calculate the possible prefix for all edge cases`() {
+        val stringArrays = listOf("dog", "racecar", "car").toTypedArray()
+        assertThat(longestCommonPrefix(stringArrays), `is`(""))
+    }
+}
