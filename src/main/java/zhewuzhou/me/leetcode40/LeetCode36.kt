23ee4ed0780f1532d4529e1c589fa78c33bc5d91
zhewuzhou <zhewuzhou@gmail.com>
2020-03-03 16:04:36 +0800

    [Z.Z][LeetCode36] finish it

--- src/main/java/zhewuzhou/me/leetcode40/LeetCode36.kt
+++ src/main/java/zhewuzhou/me/leetcode40/LeetCode36.kt
@@ -34,3 +34,40 @@ fun validBlock(board: Array<CharArray>): Boolean =
         nonEmptyChars.size == nonEmptyChars.distinct().size
     }
 
+fun solveSudoku(board: Array<CharArray>) {
+    if (board.isEmpty()) return
+    solve(board)
+}
+
+fun solve(board: Array<CharArray>): Boolean {
+    for (i in board.indices) {
+        for (j in board[0].indices) {
+            if (board[i][j] == '.') {
+                var c = '1'
+                while (c <= '9') {
+                    //trial. Try 1 through 9
+                    if (isValid(board, i, j, c)) {
+                        board[i][j] = c //Put c for this cell
+                        if (solve(board)) return true //If it's the solution return true
+                        else board[i][j] = '.' //Otherwise go back
+                    }
+                    c++
+                }
+                return false
+            }
+        }
+    }
+    return true
+}
+
+private fun isValid(board: Array<CharArray>, row: Int, col: Int, c: Char): Boolean {
+    val allBlock = ((0 + 3 * (row / 3))..(2 + 3 * (row / 3)))
+        .flatMap { r ->
+            ((0 + 3 * (col / 3))..(2 + 3 * (col / 3)))
+                .map { c -> board[r][c] }
+        }
+    return !allBlock.contains(c) &&
+        !board[row].contains(c) &&
+        !(0..8).map { board[it][col] }.contains(c)
+}
+
