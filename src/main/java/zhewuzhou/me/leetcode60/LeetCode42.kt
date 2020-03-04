953021f82718ab7319b969b53e2ce6107b76342d
zhewuzhou <zhewuzhou@gmail.com>
2020-03-04 19:48:19 +0800

    [Z.Z][LeetCode] finish the code with plain dynamic programming solution

--- src/main/java/zhewuzhou/me/leetcode60/LeetCode42.kt
+++ src/main/java/zhewuzhou/me/leetcode60/LeetCode42.kt
@@ -1,5 +1,16 @@
 package zhewuzhou.me.leetcode60
 
 fun trap(height: IntArray): Int {
-    return 0
+    val rightMax = mutableMapOf<Int, Int>()
+    val leftMax = mutableMapOf<Int, Int>()
+    var water = 0
+    for (i in 1 until height.lastIndex) {
+        leftMax[i] = Math.max(height[i - 1], (leftMax[i - 1] ?: 0))
+        val rightIndex = height.lastIndex - i
+        rightMax[rightIndex] = Math.max(height[rightIndex + 1], (rightMax[rightIndex + 1] ?: 0))
+    }
+    for (i in 1 until height.lastIndex) {
+        water += Math.max(Math.min(rightMax[i]!!, leftMax[i]!!) - height[i], 0)
+    }
+    return water
 }
