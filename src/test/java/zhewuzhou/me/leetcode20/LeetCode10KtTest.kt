9f0faaea05d2e6da54f55f59a9f27d1373aa6088
zhewuzhou <zhewuzhou@gmail.com>
2020-03-05 12:34:36 +0800

    [Z.Z][LeetCode] test bring back

--- src/test/java/zhewuzhou/me/leetcode20/LeetCode10KtTest.kt
+++ src/test/java/zhewuzhou/me/leetcode20/LeetCode10KtTest.kt
@@ -6,36 +6,36 @@ import org.junit.Test
 
 class LeetCode10KtTest {
 
-//    @Test
-//    fun `Should match for normal string`() {
-//        assertThat(isMatchRecursive("aa", "a"), `is`(false))
-//        assertThat(isMatchRecursive("aa", "a."), `is`(true))
-//    }
-//
-//    @Test
-//    fun `Should match for normal dot string`() {
-//        assertThat(isMatchRecursive("ab", ".*"), `is`(true))
-//        assertThat(isMatchRecursive("ab", ".."), `is`(true))
-//    }
+    @Test
+    fun `Should match for normal string`() {
+        assertThat(isMatchRecursive("aa", "a"), `is`(false))
+        assertThat(isMatchRecursive("aa", "a."), `is`(true))
+    }
+
+    @Test
+    fun `Should match for normal dot string`() {
+        assertThat(isMatchRecursive("ab", ".*"), `is`(true))
+        assertThat(isMatchRecursive("ab", ".."), `is`(true))
+    }
 
     @Test
     fun `Should match for normal star string`() {
-//        assertThat(isMatchRecursive("aa", "a*"), `is`(true))
+        assertThat(isMatchRecursive("aa", "a*"), `is`(true))
         assertThat(isMatchRecursive("aab", "c*a*b"), `is`(true))
-//        assertThat(isMatchRecursive("mississippi", "mis*is*p*."), `is`(false))
+        assertThat(isMatchRecursive("mississippi", "mis*is*p*."), `is`(false))
     }
 
-//    @Test
-//    fun `Should match empty src`() {
-//        assertThat(isMatchRecursive("", "a*"), `is`(true))
-//        assertThat(isMatchRecursive("", "a*b*c*"), `is`(true))
-//        assertThat(isMatchRecursive("", ""), `is`(true))
-//        assertThat(isMatchRecursive("", "a"), `is`(false))
-//        assertThat(isMatchRecursive("", "."), `is`(false))
-//    }
-//
-//    @Test
-//    fun `should match more edge cases`() {
-//        assertThat(isMatchRecursive("ab", ".*c"), `is`(false))
-//    }
+    @Test
+    fun `Should match empty src`() {
+        assertThat(isMatchRecursive("", "a*"), `is`(true))
+        assertThat(isMatchRecursive("", "a*b*c*"), `is`(true))
+        assertThat(isMatchRecursive("", ""), `is`(true))
+        assertThat(isMatchRecursive("", "a"), `is`(false))
+        assertThat(isMatchRecursive("", "."), `is`(false))
+    }
+
+    @Test
+    fun `should match more edge cases`() {
+        assertThat(isMatchRecursive("ab", ".*c"), `is`(false))
+    }
 }
