e9a38cee701ad8289ea668903da415bd75c96239
zhewuzhou <zhewuzhou@gmail.com>
2020-03-05 20:19:40 +0800

    [Z.Z][LeetCode] 47

--- src/main/java/zhewuzhou/me/leetcode60/LeetCode46.kt
+++ src/main/java/zhewuzhou/me/leetcode60/LeetCode46.kt
@@ -5,4 +5,4 @@ import zhewuzhou.me.leetcode40.allList
 fun permute(nums: IntArray): List<List<Int>> =
     allList(nums.size)
         .map { indexList -> indexList.map { nums[it] } }
-
+        .distinct()
