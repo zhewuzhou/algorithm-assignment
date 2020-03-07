914b6ebcb298b6d8dc150c5de42cc441da1d1872
zhewuzhou <zhewuzhou@gmail.com>
2020-03-07 12:37:14 +0800

    [Z.Z][LeetCode] Easy problem is not easy at all

--- src/main/java/zhewuzhou/me/leetcode60/LeetCode51.kt
+++ src/main/java/zhewuzhou/me/leetcode60/LeetCode51.kt
@@ -55,4 +55,15 @@ private fun crossCheck(comb: Array<IntArray>, row: Int, col: Int): Boolean {
     return true
 }
 
+fun maxSubArray(nums: IntArray): Int {
+    val dp = IntArray(nums.size)
+    dp[0] = nums[0]
+    var maxSub = dp[0]
+    for (i in 1..nums.lastIndex) {
+        dp[i] = nums[i] + Math.max(dp[i-1], 0)
+        maxSub = Math.max(maxSub, dp[i])
+    }
+    return maxSub
+}
+
 
