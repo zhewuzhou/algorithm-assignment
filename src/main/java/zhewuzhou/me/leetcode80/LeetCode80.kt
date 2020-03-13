6219b0096ed0ae430229efb1f0da50706cc94042
zhewuzhou <zhewuzhou@gmail.com>
2020-03-13 10:54:30 +0800

    [Z.Z][LeetCode80] Smart exchange

---
+++ src/main/java/zhewuzhou/me/leetcode80/LeetCode80.kt
@@ -0,0 +1,11 @@
+package zhewuzhou.me.leetcode80
+
+fun removeDuplicates(nums: IntArray): Int {
+    var i = 0
+    for (n in nums)
+        if (i < 2 || n > nums[i - 2]) {
+            nums[i] = n
+            i += 1
+        }
+    return i
+}
---
+++ src/test/java/zhewuzhou/me/leetcode80/LeetCode80KtTest.kt
@@ -0,0 +1,25 @@
+package zhewuzhou.me.leetcode80
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+
+class LeetCode80KtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Pair(listOf(1, 1, 1, 2, 2, 3), 5),
+                Pair(listOf(0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 3, 3), 7)
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Should remove duplications`(case: Pair<List<Int>, Int>) {
+        assertThat(removeDuplicates(case.first.toIntArray()), `is`(case.second))
+    }
+}
