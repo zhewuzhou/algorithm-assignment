7a64623d52a0d64497066aff9e7194ad43b2cfe2
zhewuzhou <zhewuzhou@gmail.com>
2020-03-20 07:52:37 +0800

    [Z.Z][LeetCode] Unique tree with DP

--- src/main/java/zhewuzhou/me/leetcode100/LeetCode94.kt
+++ src/main/java/zhewuzhou/me/leetcode100/LeetCode94.kt
@@ -23,3 +23,76 @@ fun inorderTraversal(root: TreeNode?): List<Int> {
     }
     return list
 }
+
+fun generateTrees(n: Int): List<TreeNode?> {
+    if (n == 0) return listOf()
+    return doGenerate(1, n)
+}
+
+fun numTrees(n: Int): Int {
+    val matrix = IntArray(n + 1) {
+        1
+    }
+    for (x in 2..n) {
+        matrix[x] = 0
+        for (j in 1..x) {
+            matrix[x] += matrix[j - 1] * matrix[x - j]
+        }
+    }
+    return matrix[n]
+}
+
+fun nFactor(n: Int): Int {
+    return when (n) {
+        0 -> 1
+        1 -> 1
+        else -> {
+            var x = 1
+            var res = 1
+            while (x <= n) {
+                res *= x
+                x += 1
+            }
+            res
+        }
+    }
+}
+
+fun doCount(start: Int, end: Int): Int {
+    return when (start.compareTo(end)) {
+        1 -> 1
+        0 -> 1
+        else -> {
+            var count = 0
+            for (i in start..end) {
+                val lc = doCount(start, i - 1)
+                val rc = doCount(i + 1, end)
+                count += lc * rc
+            }
+            count
+        }
+    }
+}
+
+fun doGenerate(start: Int, end: Int): List<TreeNode?> {
+    return when (start.compareTo(end)) {
+        1 -> listOf(null)
+        0 -> listOf(TreeNode(start))
+        else -> {
+            val res = mutableListOf<TreeNode?>()
+            for (i in start..end) {
+                val lp = doGenerate(start, i - 1)
+                val rp = doGenerate(i + 1, end)
+                for (l in lp) {
+                    for (r in rp) {
+                        val root = TreeNode(i)
+                        root.right = r
+                        root.left = l
+                        res.add(root)
+                    }
+                }
+            }
+            res
+        }
+    }
+}
---
+++ src/test/java/zhewuzhou/me/leetcode100/LeetCode94KtTest.kt
@@ -0,0 +1,12 @@
+package zhewuzhou.me.leetcode100
+
+import org.junit.jupiter.api.Test
+
+internal class LeetCode94KtTest {
+
+    @Test
+    fun `Should generate trees`() {
+        val res = numTrees(4)
+        println(res)
+    }
+}
