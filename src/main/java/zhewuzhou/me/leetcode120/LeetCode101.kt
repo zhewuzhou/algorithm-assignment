b7f4d3d67336f204692168fd066e01742692103e
zhewuzhou <zhewuzhou@gmail.com>
2020-03-21 21:35:21 +0800

    [Z.Z][LeetsCode] Implement the solution with recursive approaches

--- src/main/java/zhewuzhou/me/leetcode120/LeetCode101.kt
+++ src/main/java/zhewuzhou/me/leetcode120/LeetCode101.kt
@@ -3,5 +3,15 @@ package zhewuzhou.me.leetcode120
 import zhewuzhou.me.leetcode100.TreeNode
 
 fun isSymmetric(root: TreeNode?): Boolean {
+    return isSymmetricRecursive(root?.left, root?.right)
+}
+
+
+fun isSymmetricRecursive(lhs: TreeNode?, rhs: TreeNode?): Boolean {
+    if (lhs == null && rhs == null) return true
+    if (lhs == null || rhs == null) return false
+    if (lhs.`val` == rhs.`val`) {
+        return isSymmetricRecursive(lhs.right, rhs.left) && isSymmetricRecursive(lhs.left, rhs.right)
+    }
     return false
 }
