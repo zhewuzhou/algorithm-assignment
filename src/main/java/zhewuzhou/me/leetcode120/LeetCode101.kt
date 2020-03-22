e73aed5afaf7b843005bbfdaff5f3c3daf811b4d
zhewuzhou <zhewuzhou@gmail.com>
2020-03-22 21:37:08 +0800

    [Z.Z][BFS tree in normal/zigzag] y

--- src/main/java/zhewuzhou/me/leetcode120/LeetCode101.kt
+++ src/main/java/zhewuzhou/me/leetcode120/LeetCode101.kt
@@ -43,3 +43,39 @@ fun isSymmetricRecursive(lhs: TreeNode?, rhs: TreeNode?): Boolean {
     }
     return false
 }
+
+fun levelOrder(root: TreeNode?): List<List<Int>> {
+    return travel(root, false)
+}
+
+private fun travel(root: TreeNode?, isZigZag: Boolean): List<List<Int>> {
+    if (root == null) return listOf()
+    var zigzag = isZigZag
+    val queue = LinkedList<TreeNode>()
+    queue.add(root)
+    val res = mutableListOf(listOf(root.`val`))
+    while (queue.isNotEmpty()) {
+        val level = mutableListOf<TreeNode>()
+        while (queue.isNotEmpty()) {
+            val ele = queue.pop()
+            if (ele.left != null) {
+                level.add(ele.left!!)
+            }
+            if (ele.right != null) {
+                level.add(ele.right!!)
+            }
+        }
+        if (level.isNotEmpty()) {
+            if (isZigZag && zigzag) {
+                res.add(level.reversed().map { it.`val` })
+            }
+            queue.addAll(level)
+        }
+        if (isZigZag) zigzag = !zigzag
+    }
+    return res
+}
+
+fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+    return travel(root, true)
+}
