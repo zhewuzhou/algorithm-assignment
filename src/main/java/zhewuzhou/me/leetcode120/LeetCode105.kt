86970a02412f6fee0189776b0a4dd38e9d22ae15
zhewuzhou <zhewuzhou@gmail.com>
2020-03-25 10:34:45 +0800

    [Z.Z][LeetCode] sorted array to balanced tree

--- src/main/java/zhewuzhou/me/leetcode120/LeetCode105.kt
+++ src/main/java/zhewuzhou/me/leetcode120/LeetCode105.kt
@@ -6,17 +6,27 @@ fun buildTree(preorder: IntArray, inorder: IntArray): TreeNode? {
     if (preorder.isEmpty() || inorder.isEmpty() || preorder.sorted() != inorder.sorted()) return null
     if (preorder.size == 1) return TreeNode(preorder[0])
     val root = TreeNode(preorder[0])
-    val (inLeft, inRight) = splitInorder(inorder, root.`val`)
+    val (inLeft, inRight) = splitArray(inorder, root.`val`)
     root.left = buildTree(preorder.filter { inLeft.contains(it) }.toIntArray(), inLeft)
     root.right = buildTree(preorder.filter { inRight.contains(it) }.toIntArray(), inRight)
     return root
 }
 
-private fun splitInorder(inorder: IntArray, root: Int): Pair<IntArray, IntArray> {
-    val mid = inorder.indexOf(root)
-    val inLeft = inorder.copyOfRange(0, mid)
-    val inRight = inorder.copyOfRange(mid + 1, inorder.size)
+private fun splitArray(target: IntArray, root: Int): Pair<IntArray, IntArray> {
+    val mid = target.indexOf(root)
+    val inLeft = target.copyOfRange(0, mid)
+    val inRight = target.copyOfRange(mid + 1, target.size)
     return Pair(inLeft, inRight)
+}
+
+fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    if (nums.isEmpty()) return null
+    if (nums.size == 1) return TreeNode(nums[0])
+    val root = TreeNode(nums[(0 + nums.size) / 2])
+    val (left, right) = splitArray(nums, root.`val`)
+    root.left = sortedArrayToBST(left)
+    root.right = sortedArrayToBST(right)
+    return root
 }
 
 
