44913e3cdcf8ed3092a827a8cb50f32f0e82cfc7
zhewuzhou <zhewuzhou@gmail.com>
2020-03-26 13:09:02 +0800

    [Z.Z][LeetCode] Dummy head for linked situation

--- src/main/java/zhewuzhou/me/leetcode120/LeetCode116.kt
+++ src/main/java/zhewuzhou/me/leetcode120/LeetCode116.kt
@@ -21,4 +21,24 @@ fun connectNeighbors(lhs: Node?, rhs: Node?) {
     connectNeighbors(lhs.right, rhs.left)
 }
 
+fun connect(root: Node?): Node? {
+    var curRoot = root
+    while (curRoot != null) {
+        val dummyHead = Node(-1)
+        var curr: Node? = dummyHead
+        while (curRoot != null) {
+            if (curRoot.left != null) {
+                curr?.next = curRoot.left
+                curr = curr?.next
+            }
+            if (curRoot.right != null) {
+                curr?.next = curRoot.right
+                curr = curr?.next
+            }
+            curRoot = curRoot.next
+        }
+        curRoot = dummyHead.next
+    }
+    return root
+}
 
