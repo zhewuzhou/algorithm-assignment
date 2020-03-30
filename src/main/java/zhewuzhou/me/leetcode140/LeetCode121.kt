02cc153687f13e3b6df1c9a824e79726672275f6
zhewuzhou <zhewuzhou@gmail.com>
2020-03-30 21:06:04 +0800

    [Z.Z][LeetCode] setup test

--- src/main/java/zhewuzhou/me/leetcode140/LeetCode121.kt
+++ src/main/java/zhewuzhou/me/leetcode140/LeetCode121.kt
@@ -18,16 +18,20 @@ fun maxProfitQ(prices: IntArray): Int {
     return maxProfit
 }
 
-fun maxProfit(prices: IntArray): Int {
-    if (prices.size < 2) return 0
+fun maxProfitOneTransaction(prices: IntArray, start: Int, end: Int): Int {
+    if (end < 2) return 0
     var maxProfit = 0
     var minPrice = prices[0]
-    for (i in 1 until prices.size) {
-        if (prices[i] > prices[i - 1]) {
+    for (i in start until end) {
+        if (prices[i] > prices[i - start]) {
             maxProfit = Math.max(maxProfit, prices[i] - minPrice)
         } else {
             minPrice = Math.min(minPrice, prices[i])
         }
     }
     return maxProfit
+}
+
+fun maxProfit(prices: IntArray): Int {
+    return 0
 }
--- src/test/java/zhewuzhou/me/leetcode140/LeetCode121KtTest.kt
+++ src/test/java/zhewuzhou/me/leetcode140/LeetCode121KtTest.kt
@@ -9,17 +9,33 @@ import java.util.*
 internal class LeetCode121KtTest {
     private companion object {
         @JvmStatic
-        fun cases() = Arrays.stream(
+        fun cases1T() = Arrays.stream(
             arrayOf(
                 Pair(listOf(7, 1, 5, 3, 6, 4), 5),
+                Pair(listOf(7, 6, 4, 3, 1), 0)
+            )
+        )
+
+        @JvmStatic
+        fun cases2T() = Arrays.stream(
+            arrayOf(
+                Pair(listOf(3, 3, 5, 0, 0, 3, 1, 4), 6),
+                Pair(listOf(1, 2, 3, 4, 5), 0),
                 Pair(listOf(7, 6, 4, 3, 1), 0)
             )
         )
     }
 
     @ParameterizedTest
-    @MethodSource("cases")
+    @MethodSource("cases2T")
     fun `Should calculate the maximum profit`(case: Pair<List<Int>, Int>) {
+        val source = case.first.toIntArray()
+        assertThat(maxProfitOneTransaction(source, 1, source.size), `is`(case.second))
+    }
+
+    @ParameterizedTest
+    @MethodSource("cases1T")
+    fun `Should calculate the maximum profit given at most 2 T`(case: Pair<List<Int>, Int>) {
         val source = case.first.toIntArray()
         assertThat(maxProfit(source), `is`(case.second))
     }
