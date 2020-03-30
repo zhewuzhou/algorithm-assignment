ca04660aba6798f83d1257df3d7a16fcb927da07
zhewuzhou <zhewuzhou@gmail.com>
2020-03-30 22:13:32 +0800

    [Z.Z][LeetCode] Finish 2 eggs issues

--- src/main/java/zhewuzhou/me/leetcode140/TwoEggs.kt
+++ src/main/java/zhewuzhou/me/leetcode140/TwoEggs.kt
@@ -1,2 +1,27 @@
 package zhewuzhou.me.leetcode140
 
+//egg not larger than log(l) or else lose meaning
+fun eggThrowDP(egg: Int, level: Int): Int {
+    val caches = mutableMapOf<Pair<Int, Int>, Int>()
+    fun eggThrow(e: Int, l: Int): Int {
+        if (caches.containsKey(Pair(e, l))) {
+            return caches[Pair(e, l)]!!
+        }
+        var res = l
+        when {
+            e == 1 -> res = l
+            l == 1 -> res = 1
+            else -> {
+                for (k in 1..l) {
+                    val possible = Math.max(eggThrow(e - 1, k), eggThrow(e, l - k)) + 1
+                    if (res > possible) {
+                        res = possible
+                    }
+                }
+            }
+        }
+        caches[Pair(e, l)] = res
+        return res
+    }
+    return eggThrow(egg, level)
+}
---
+++ src/test/java/zhewuzhou/me/leetcode140/TwoEggsKtTest.kt
@@ -0,0 +1,12 @@
+package zhewuzhou.me.leetcode140
+
+import org.junit.jupiter.api.Test
+
+internal class TwoEggsKtTest {
+
+    @Test
+    fun `Throw egg problem solve with DP`() {
+        val result = eggThrowDP(20, 100)
+        println(result)
+    }
+}
