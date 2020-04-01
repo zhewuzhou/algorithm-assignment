8470ae1be82ab33aa88fc368663b964d647bb836
zhewuzhou <zhewuzhou@gmail.com>
2020-04-01 14:42:05 +0800

    [Z.Z][LeetCode] more problems solved

---
+++ src/main/java/zhewuzhou/me/leetcode140/LeetCode124.kt
@@ -0,0 +1,22 @@
+package zhewuzhou.me.leetcode140
+
+import zhewuzhou.me.leetcode100.TreeNode
+
+fun maxPathSum(root: TreeNode?): Int {
+    var maxValue = Int.MIN_VALUE
+    fun maxPathDown(node: TreeNode?): Int {
+        if (node == null) return 0
+        val left: Int = Math.max(0, maxPathDown(node.left))
+        val right: Int = Math.max(0, maxPathDown(node.right))
+        maxValue = Math.max(maxValue, left + right + node.`val`)
+        return Math.max(left, right) + node.`val`
+    }
+    maxPathDown(root);
+    return maxValue;
+}
+
+fun isPalindrome(s: String): Boolean {
+    if (s.isEmpty()) return true
+    val ts = s.toCharArray().filter { (it in 'a'..'z') || (it in 'A'..'Z') }.map { it.toUpperCase() }.joinToString()
+    return ts == ts.reversed()
+}
