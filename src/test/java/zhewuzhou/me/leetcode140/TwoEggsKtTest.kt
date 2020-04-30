3e6b0e0f98f15992edc26c4cf6ffbc2f5e0f0f53
zhewuzhou <zhewuzhou@gmail.com>
2020-04-30 22:00:43 +0800

    [Z.Z][Refactor] Make egg throw parameterlized test

--- src/test/java/zhewuzhou/me/leetcode140/TwoEggsKtTest.kt
+++ src/test/java/zhewuzhou/me/leetcode140/TwoEggsKtTest.kt
@@ -1,12 +1,30 @@
 package zhewuzhou.me.leetcode140
 
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
 
 internal class TwoEggsKtTest {
+    private companion object {
+        @JvmStatic
+        fun cases() = Arrays.stream(
+            arrayOf(
+                Triple(1, 100, 100),
+                Triple(2, 100, 15),
+                Triple(3, 100, 10),
+                Triple(4, 100, 9),
+                Triple(5, 100, 8),
+                Triple(20, 100, 8)
+            )
+        )
+    }
 
-    @Test
-    fun `Throw egg problem solve with DP`() {
-        val result = eggThrowDP(20, 100)
-        println(result)
+    @ParameterizedTest
+    @MethodSource("cases")
+    fun `Throw egg problem solve with DP`(case: Triple<Int, Int, Int>) {
+        assertThat(eggThrowDP(case.first, case.second), `is`(case.third))
     }
 }
