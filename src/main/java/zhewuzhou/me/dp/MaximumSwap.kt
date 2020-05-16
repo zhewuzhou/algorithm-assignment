b8e133c1ad9aa55a5342846b4ffb634e338bba02
zhewuzhou <zhewuzhou@gmail.com>
2020-05-16 23:11:03 +0800

    [Z.Z][Add a quick solution] y

--- src/main/java/zhewuzhou/me/dp/MaximumSwap.kt
+++ src/main/java/zhewuzhou/me/dp/MaximumSwap.kt
@@ -1,6 +1,6 @@
 package zhewuzhou.me.dp
 
-fun maximumSwap(num: Int): Int {
+fun maximumSwapSlow(num: Int): Int {
     if (num < 10) return num
     val digits = splitToDigits(num)
     for (i in digits.lastIndex downTo 1) {
@@ -10,6 +10,25 @@ fun maximumSwap(num: Int): Int {
             digits[i] = max.first
             digits[max.second] = cur
             return toInt(digits)
+        }
+    }
+    return num
+}
+
+fun maximumSwap(num: Int): Int {
+    val digits = num.toString().toCharArray()
+    val buckets = IntArray(10)
+    for (i in digits.indices) {
+        buckets[digits[i] - '0'] = i
+    }
+    for (i in digits.indices) {
+        for (k in 9 downTo digits[i] - '0' + 1) {
+            if (buckets[k] > i) {
+                val tmp = digits[i]
+                digits[i] = digits[buckets[k]]
+                digits[buckets[k]] = tmp
+                return Integer.valueOf(String(digits))
+            }
         }
     }
     return num
