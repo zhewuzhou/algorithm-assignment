5647d3c4e8c69c35794f6168b1052845c32cfb6d
zhewuzhou <zhewuzhou@gmail.com>
2020-05-20 22:56:11 +0800

    [Z.Z][LeetCode] Create maximum number with divide and conquare

---
+++ src/main/java/zhewuzhou/me/dc/CreateMaximumNumber321.kt
@@ -0,0 +1,95 @@
+package zhewuzhou.me.dc
+
+fun maxNumber(nums1: IntArray, nums2: IntArray, k: Int): IntArray {
+    var lhs = nums1
+    var rhs = nums2
+    if (nums1.size > nums2.size) {
+        lhs = nums2
+        rhs = nums1
+    }
+    return when (k.compareTo(nums1.size + nums2.size)) {
+        0 -> mergeArray(nums1, nums2)
+        1 -> intArrayOf()
+        else -> {
+            var maxArray = IntArray(k) {
+                -1
+            }
+            for (i in Math.max(0, k - rhs.size)..Math.min(lhs.size, k)) {
+                val p = mergeArray(pickMaximum(lhs, i), pickMaximum(rhs, k - i))
+                if (compareArray(p, maxArray, 0, 0)) {
+                    maxArray = p
+                }
+            }
+            maxArray
+        }
+    }
+}
+
+fun compareArray(lhs: IntArray, rhs: IntArray, lStart: Int, rStart: Int): Boolean {
+    for (i in 0..(lhs.lastIndex - lStart)) {
+        when (lhs[lStart + i].compareTo(if (rStart + i > rhs.lastIndex) 0 else rhs[rStart + i])) {
+            1 -> return true
+            -1 -> return false
+        }
+    }
+    return true
+}
+
+fun mergeArray(lhs: IntArray, rhs: IntArray): IntArray {
+    val result = mutableListOf<Int>()
+    var li = 0
+    var ri = 0
+    while (li <= lhs.lastIndex || ri <= rhs.lastIndex) {
+        when {
+            li == lhs.size -> {
+                result.addAll(rhs.copyOfRange(ri, rhs.size).toList())
+                ri = rhs.size
+            }
+            ri == rhs.size -> {
+                result.addAll(lhs.copyOfRange(li, lhs.size).toList())
+                li = lhs.size
+            }
+            else -> {
+                when (lhs[li].compareTo(rhs[ri])) {
+                    0 -> {
+                        if (compareArray(rhs, lhs, li, ri)) {
+                            result.add(lhs[li])
+                            li += 1
+                        } else {
+                            result.add(rhs[ri])
+                            ri += 1
+                        }
+                    }
+                    1 -> {
+                        result.add(lhs[li])
+                        li += 1
+                    }
+                    -1 -> {
+                        result.add(rhs[ri])
+                        ri += 1
+                    }
+                }
+            }
+        }
+    }
+    return result.toIntArray()
+}
+
+fun pickMaximum(nums: IntArray, k: Int): IntArray {
+    if (nums.size == k) return nums.copyOf()
+    if (k == 0) return intArrayOf()
+    val result = mutableListOf<Int>()
+    fun pick(start: Int, count: Int) {
+        var max = start
+        for (i in start..Math.min(nums.lastIndex, nums.lastIndex - count + 1)) {
+            if (nums[i] > nums[max]) {
+                max = i
+            }
+        }
+        result.add(nums[max])
+        if (count > 1)
+            pick(max + 1, count - 1)
+    }
+    pick(0, k)
+    return result.toIntArray()
+}
