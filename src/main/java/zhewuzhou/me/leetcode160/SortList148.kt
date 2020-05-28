60eee847b7cbf3fe24e674f3dc6876ab8667d1ce
zhewuzhou <zhewuzhou@gmail.com>
2020-05-28 23:01:33 +0800

    [Z.Z][LeetCode] insert srot for linked list

--- src/main/java/zhewuzhou/me/leetcode160/SortList148.kt
+++ src/main/java/zhewuzhou/me/leetcode160/SortList148.kt
@@ -52,3 +52,27 @@ fun merge(l1: ListNode?, l2: ListNode?): ListNode? {
     }
     return dummyHead.next
 }
+
+fun insertionSortList(head: ListNode?): ListNode? {
+    if (head?.next == null) {
+        return head
+    }
+    val helper = ListNode(0) //new starter of the sorted list
+    var cur = head //the node will be inserted
+    var pre: ListNode? = helper //insert node between pre and pre.next
+    var next: ListNode? = null //the next node will be inserted
+    //not the end of input list
+    while (cur != null) {
+        next = cur.next
+        //find the right place to insert
+        while (pre!!.next != null && pre.next!!.`val` < cur.`val`) {
+            pre = pre.next
+        }
+        //insert between pre and pre.next
+        cur.next = pre.next
+        pre.next = cur
+        pre = helper
+        cur = next
+    }
+    return helper.next
+}
