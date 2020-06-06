250a3e009575e94cf5b5b046df75c6bb38d0f1e8
zhewuzhou <zhewuzhou@gmail.com>
2020-06-06 11:04:25 +0800

    [Z.Z][LeetCode] Bit operation is really awsome

--- src/main/java/zhewuzhou/me/leetcode200/LeetCode189.kt
+++ src/main/java/zhewuzhou/me/leetcode200/LeetCode189.kt
@@ -20,3 +20,15 @@ private fun reverse(nums: IntArray, start: Int, end: Int) {
         e -= 1
     }
 }
+
+fun reverseBits(n: Int): Int {
+    var num = n
+    if (num == 0) return 0
+    var result = 0
+    for (i in 0..31) {
+        result = result shl 1
+        if (num and 1 == 1) result++
+        num = num shr 1
+    }
+    return result
+}
