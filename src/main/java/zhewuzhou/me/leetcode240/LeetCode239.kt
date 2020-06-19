253440c50757008b31a994b14fa43e2402543a79
zhewuzhou <zhewuzhou@gmail.com>
2020-06-19 11:39:15 +0800

    [Z.Z][LeetCode] Yet another solution with treemap

--- src/main/java/zhewuzhou/me/leetcode240/LeetCode239.kt
+++ src/main/java/zhewuzhou/me/leetcode240/LeetCode239.kt
@@ -2,7 +2,7 @@ package zhewuzhou.me.leetcode240
 
 import java.util.*
 
-fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+fun maxSlidingWindowQ(nums: IntArray, k: Int): IntArray {
     if (nums.isEmpty() || k == 0) return intArrayOf()
     val result = IntArray(nums.size - k + 1)
     val win = LinkedList<Int>()
@@ -16,6 +16,30 @@ fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
         win.offerLast(i)
         if (i >= k - 1) {
             result[i - k + 1] = nums[win.peekFirst()]
+        }
+    }
+    return result
+}
+
+fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+    if (nums.isEmpty() || k == 0) return intArrayOf()
+    val result = IntArray(nums.size - k + 1)
+    val window = TreeMap<Int, Int>()
+    for (i in nums.indices) {
+        if (window.containsKey(nums[i])) {
+            window[nums[i]] = window[nums[i]]!! + 1
+        } else {
+            window[nums[i]] = 1
+        }
+        if (i >= k) {
+            if (window[nums[i - k]]!! > 1) {
+                window[nums[i - k]] = window[nums[i - k]]!! - 1
+            } else {
+                window.remove(nums[i - k])
+            }
+        }
+        if (i >= k - 1) {
+            result[i - k + 1] = window.lastKey()
         }
     }
     return result
